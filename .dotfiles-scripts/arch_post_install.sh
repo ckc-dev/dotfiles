a45798987c8315b5f6c67f87707746cdacd6af9a
ckc-dev <70550797+ckc-dev@users.noreply.github.com>
2022-03-15 08:08:37 -0300

    Update Arch post install script.
    Updated Arch post installation script to fix a small issue with the AUR helper installation and update the repository URI.

--- .dotfiles-scripts/arch_post_install.sh
+++ .dotfiles-scripts/arch_post_install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# NOTE: THIS SCRIPT IS MEANT FOR FRESH INSTALLS.
+# NOTE: THIS SCRIPT IS MEANT FOR FRESH ARCH LINUX INSTALLS.
 # This script will install programs and override configuration
 # files, make sure this is what you want to do.
 
 set -e
+
+REPOSITORY_URI="https://github.com/ckc-dev/dotfiles.git"
+REPOSITORY_URI_SSH="git@github.com:ckc-dev/dotfiles.git"
 
 printf "Step 1: Enabling multilib repositiories on pacman...\n"
 sudo bash -c "printf '\n\n[multilib]\nInclude = /etc/pacman.d/mirrorlist\n' >> /etc/pacman.conf"
@@ -17,10 +20,11 @@ printf "\nStep 3: Setting up dotfiles...\n"
 printf "Creating '.gitgnore'...\n"
 printf ".dotfiles" >> $HOME/.gitignore
 
-read -p "Dotfiles repository URL: " REPOSITORY_URL
-
 printf "Cloning repository...\n"
-git clone --bare $REPOSITORY_URL $HOME/.dotfiles
+git clone --bare $REPOSITORY_URI $HOME/.dotfiles
+
+printf "Changing repository HTTPS URI to SSH...\n"
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote set-url origin $REPOSITORY_URL_SSH
 
 printf "Configuring repository...\n"
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout --force
@@ -33,11 +37,12 @@ printf "Generating configuration files...\n"
 python $HOME/.dotfiles-pytheme/pytheme.py
 
 printf "\nStep 4: Installing AUR helper...\n"
-git clone https://aur.archlinux.org/paru.git /tmp/
-cd /tmp/paru/
+mkdir /tmp/aur/
+git clone https://aur.archlinux.org/paru.git /tmp/aur/
+cd /tmp/aur/paru/
 makepkg -si
 cd -
-rm -rf /tmp/paru/
+rm -rf /tmp/aur/
 
 printf "\nStep 5: Installing AUR packages...\n"
 paru visual-studio-code-bin
