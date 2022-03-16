3e428a064260c145517f623eef95d22b0923962b
ckc-dev <70550797+ckc-dev@users.noreply.github.com>
2022-03-16 13:24:55 -0300

    Add Arch post install script for a Lenovo s145.

---
+++ .dotfiles-scripts/arch_post_install_lenovo_s145.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# NOTE: THIS SCRIPT IS MEANT FOR FRESH ARCH LINUX INSTALLS.
+# This will override configurations with sensible (perhaps a
+# bit biased) values for a Lenovo s145 notebook.
+
+set -e
+
+DOTFILES_URI="https://github.com/ckc-dev/dotfiles.git"
+
+printf "Running initial, general Arch post install process...\n"
+./arch-post-install.sh
+
+printf "Cloning s145 configuration files into a temporary folder...\n"
+mkdir /tmp/s145/
+git clone $DOTFILES_URI /tmp/s145/ -b lenovo-s145
+
+printf "Copying s145 configuration files...\n"
+sudo cp -r /tmp/s145/etc/* /etc/
+
+printf "Deleting temporary folder used for configuration files..."
+rm -rf /tmp/s145/
