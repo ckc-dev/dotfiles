41dacbb86d940b5172113d59b7acd9040181a0f4
ckc-dev <70550797+ckc-dev@users.noreply.github.com>
2022-12-18 22:24:20 -0300

    Fix wrong program path.

--- .dotfiles-scripts/arch_post_install.sh
+++ .dotfiles-scripts/arch_post_install.sh
@@ -6,9 +6,6 @@
 
 set -e
 
-REPOSITORY_URI="https://github.com/ckc-dev/dotfiles.git"
-REPOSITORY_URI_SSH="git@github.com:ckc-dev/dotfiles.git"
-
 printf "Step 1: Enabling multilib repositiories on pacman...\n"
 sudo bash -c "printf '\n\n[multilib]\nInclude = /etc/pacman.d/mirrorlist\n' >> /etc/pacman.conf"
 
@@ -16,25 +13,10 @@ printf "\nStep 2: Installing packages...\n"
 sudo pacman -Syu acpilight base-devel darktable firefox gcc gimp i3-gaps i3status nomacs noto-fonts noto-fonts-cjk noto-fonts-emoji ntfs-3g openssh pavucontrol pulseaudio python python-pip rofi rxvt-unicode steam vlc xorg-server xorg-xinit xorg-xsetroot hdparm pcmanfm-gtk3 flameshot android-file-transfer playerctl qbittorrent dotnet-runtime dotnet-sdk udisks udiskie
 
 printf "\nStep 3: Setting up dotfiles...\n"
-
-printf "Creating '.gitgnore'...\n"
-printf ".dotfiles" >> $HOME/.gitignore
-
-printf "Cloning repository...\n"
-git clone --bare $REPOSITORY_URI $HOME/.dotfiles
-
-printf "Changing repository HTTPS URI to SSH...\n"
-git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote set-url origin $REPOSITORY_URI_SSH
-
-printf "Configuring repository...\n"
-git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout --force
-git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
-
-printf "Deleting '.gitignore'...\n"
-rm $HOME/.gitignore
+bash setup_dotfiles.sh
 
 printf "Generating configuration files...\n"
-python $HOME/.dotfiles-pytheme/pytheme.py
+python $HOME/.dotfiles-pyconfig/pytheme.py -x termux-colors
 
 printf "\nStep 4: Installing AUR helper...\n"
 mkdir /tmp/aur/
