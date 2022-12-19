7f35818705c49a59cbe63700161559dca1624c48
ckc-dev <70550797+ckc-dev@users.noreply.github.com>
2022-12-18 22:46:39 -0300

    Fix missing URIs.

--- .dotfiles-scripts/setup_dotfiles.sh
+++ .dotfiles-scripts/setup_dotfiles.sh
@@ -1,11 +1,14 @@
+REPOSITORY_HTTPS="https://github.com/ckc-dev/dotfiles.git"
+REPOSITORY_SSH="git@github.com:ckc-dev/dotfiles.git"
+
 printf "Creating '.gitgnore'...\n"
 printf ".dotfiles" >> $HOME/.gitignore
 
 printf "Cloning repository...\n"
-git clone --bare $REPOSITORY_URI $HOME/.dotfiles
+git clone --bare $REPOSITORY_HTTPS $HOME/.dotfiles
 
 printf "Changing repository HTTPS URI to SSH...\n"
-git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote set-url origin $REPOSITORY_URI_SSH
+git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote set-url origin $REPOSITORY_SSH
 
 printf "Configuring repository...\n"
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout --force
