a74f7de3ea9036f829dd1c6776a4c2c6303efe32
ckc-dev <70550797+ckc-dev@users.noreply.github.com>
2022-12-20 13:38:27 -0300

    Standardize whitespaces.

--- .dotfiles-scripts/termux_post_install.sh
+++ .dotfiles-scripts/termux_post_install.sh
@@ -22,15 +22,14 @@ FONT_DESTINATION=$HOME/.termux/font.ttf
 FONT_SOURCE_LOCATION=variable_ttf/FiraCode-VF.ttf
 GITHUB_API_FONT_REPO_LATEST_RELEASE_URL=https://api.github.com/repos/tonsky/FiraCode/releases/latest
 DOWNLOAD_URL=$(curl $GITHUB_API_FONT_REPO_LATEST_RELEASE_URL\
-	| grep "browser_download_url.*.zip"\
-	| cut -d : -f 2,3\
-	| tr -d \"
+  | grep "browser_download_url.*.zip"\
+  | cut -d : -f 2,3\
+  | tr -d \"
 )
 
 wget -O $TEMP_DIR/font.zip $DOWNLOAD_URL
 unzip $TEMP_DIR/font.zip -d $TEMP_DIR/font/
 cp $TEMP_DIR/font/$FONT_SOURCE_LOCATION $FONT_DESTINATION
-
 
 # Set up dotfiles.
 bash setup_dotfiles.sh
