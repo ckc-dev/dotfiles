2e75666d4f11d44181f65fc5db4517af0cf8a4f6
ckc-dev <70550797+ckc-dev@users.noreply.github.com>
2023-07-26 10:00:17 -0300

    [REFACTOR]: Delete nvim dotfiles.
    They were not great, so, for now, it's better not to use those.

--- .config/nvim/after/plugin/treesitter.lua
+++
@@ -1,7 +0,0 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = "all",
-  ignore_install = { "kdl" },
-  sync_install = true,
-  auto_install = true,
-  highlight = { enable = true },
-}
