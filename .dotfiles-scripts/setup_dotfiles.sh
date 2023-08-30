fa83b347c77641c1846f1943a06b3998d7b28e6d
ckc-dev <70550797+ckc-dev@users.noreply.github.com>
2023-08-30 15:32:24 +0000

    [FIX]: Fix dotfiles path.

--- .dotfiles-scripts/setup_dotfiles.sh
+++ .dotfiles-scripts/setup_dotfiles.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# This script sets up a bare dotfiles repository.
 
 # Exit immediately if any command exits with a non-zero status or an unset variable is used.
 set -eu
 
 repository_url_https="https://github.com/ckc-dev/dotfiles.git"
 repository_url_ssh="git@github.com:ckc-dev/dotfiles.git"
-dotfiles_path="$dotfiles_path"
+dotfiles_path="$HOME/.dotfiles"
 
 printf "Cloning repository...\n"
 git clone $repository_url_https $dotfiles_path
