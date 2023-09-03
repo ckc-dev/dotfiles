ec794724e24aa50968cacf64ec91584b7f644404
ckc-dev <70550797+ckc-dev@users.noreply.github.com>
2023-09-03 06:29:54 -0700

    [FIX]: Fixed and majorly improved container setup.

--- scripts/setup_containers.sh
+++ scripts/setup_containers.sh
@@ -1,29 +1,55 @@
 #!/bin/bash
+
 dotfiles_dir="$HOME/.dotfiles"
 containers_dir=".containers"
+target_dir="$HOME/$containers_dir"
+verbose=false
 
-# Create the base folder structure.
-mkdir -p "$HOME/$containers_dir"
+# Function to create hard links for files and directories
+create_hard_links() {
+    local source_dir="$1"
+    local target_dir="$2"
 
-# Find all compose.yaml files in .dotfiles/.containers.
-mapfile -t compose_files < <(find "$dotfiles_dir/$containers_dir" -name "compose.yaml")
+    # Loop through the items in the source directory
+    for item in "$source_dir"/*; do
+        # Get the item's base name (file or directory name)
+        local base_name="$(basename "$item")"
 
-# Loop through the compose files and create symbolic links.
-for file in "${compose_files[@]}"; do
-  source_folder=$(dirname "${file#$dotfiles_dir/$containers_dir}")
-  target_folder="$HOME/$containers_dir/$source_folder"
+        if [ -e "$item" ]; then
+            if [ -d "$item" ]; then
+                # Recursively create a directory in the target location
+                mkdir -p "$target_dir/$base_name"
+                if [ "$verbose" = true ]; then
+                    echo "Created directory: $target_dir/$base_name"
+                fi
+                create_hard_links "$item" "$target_dir/$base_name"
+            elif [ -f "$item" ]; then
+                # Create a hard link for regular files
+                ln "$item" "$target_dir/$base_name"
+                if [ "$verbose" = true ]; then
+                    echo "Created hard link for: $item"
+                fi
+            fi
+        elif [ "$verbose" = true ]; then
+            echo "Skipping non-existent item: $item"
+        fi
+    done
+}
 
-  # Create symbolic link for compose.yaml.
-  mkdir -p "$target_folder"
-  ln -s "$file" "$target_folder/"
-
-  # Create target config folder and link contents.
-  config_source="$dotfiles_dir/$containers_dir/$source_folder/config"
-  config_target="$target_folder/config"
-  if [ -d "$config_source" ]; then
-    mkdir -p "$config_target"
-    find "$config_source" -maxdepth 1 ! -name "config" -exec ln -s "{}" "$config_target/" \;
-  fi
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -v|--verbose)
+            verbose=true
+            shift
+            ;;
+        *)
+            echo "Invalid option: $1" >&2
+            exit 1
+            ;;
+    esac
 done
 
-echo "Folder structure, symbolic links, and config links created."
+# Call the function to create hard links
+create_hard_links "$dotfiles_dir/$containers_dir" "$target_dir"
+
+echo "Folder structure with hard links created."
