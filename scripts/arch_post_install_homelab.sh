2f0e219ac36506c6a4543d7b86a97780fcdf63df
ckc-dev <70550797+ckc-dev@users.noreply.github.com>
2023-09-03 00:16:20 +0000

    [FEAT]: Set up services and run scripts.

--- scripts/arch_post_install_homelab.sh
+++ scripts/arch_post_install_homelab.sh
@@ -5,6 +5,8 @@
 # Exit immediately if any command exits with a non-zero status or an unset variable is used.
 set -eu
 
+DOTFILES_DIR="$HOME/.dotfiles/scripts"
+
 read -p "This script will install packages and modify system configurations. Are you sure you want to continue? (y/n) " -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     exit 1
@@ -22,5 +24,15 @@ packages=(
     udisks2
 )
 
+printf "\nSetting up dotfiles...\n"
+bash $SCRIPTS_DIR/setup_dotfiles.sh
+
 printf "\nInstalling packages...\n"
 pacman -Syu "${packages[@]}"
+
+printf "\Enabling services...\n"
+systemctl enable sshd
+systemctl enable docker
+
+systemctl start sshd
+systemctl start docker
