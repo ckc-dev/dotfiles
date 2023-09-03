9006b5783115c197d0a3a435ecd12d9c87025932
ckc-dev <70550797+ckc-dev@users.noreply.github.com>
2023-09-03 00:15:32 +0000

    [DOCS]: Better documentation.

--- scripts/arch_post_install_homelab.sh
+++ scripts/arch_post_install_homelab.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 # This script installs and sets up a variety of packages and configurations on a fresh Arch Linux installation.
+# This is aimed at a homelab server Arch setup, and presumes that a root user is being used.
 
+# Exit immediately if any command exits with a non-zero status or an unset variable is used.
 set -eu
 
+read -p "This script will install packages and modify system configurations. Are you sure you want to continue? (y/n) " -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+fi
+
+# Install packages.
 packages=(
     docker
     docker-compose
     openssh
 )
 
+printf "\nInstalling packages...\n"
 pacman -Syu "${packages[@]}"
