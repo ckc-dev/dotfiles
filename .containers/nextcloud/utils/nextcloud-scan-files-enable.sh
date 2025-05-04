da5c77ebf6a1dc777574c90bc3dbf2a4cd866a06
ckc-dev <70550797+ckc-dev@users.noreply.github.com>
2025-05-04 14:53:00 -0300

    [FEAT]: Add script to facilitate enabling Nextcloud service.

---
+++ .containers/nextcloud/utils/nextcloud-scan-files-enable.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This script sets up a systemd timer to periodically run the Nextcloud occ files:scan command.
+
+NEXTCLOUD_UTILS_LOCATION=$HOME/.dotfiles/.containers/nextcloud/utils
+
+# --- Link Unit Files ---
+
+# Link service and timer files.
+sudo ln -s ${NEXTCLOUD_UTILS_LOCATION}/nextcloud-scan-files.service /etc/systemd/system/
+sudo ln -s ${NEXTCLOUD_UTILS_LOCATION}/nextcloud-scan-files.timer /etc/systemd/system/
+
+# Ensure that systemd reads the newly linked files.
+sudo systemctl daemon-reload
+
+# Start and enable the timer.
+sudo systemctl start nextcloud-scan-files.timer
+sudo systemctl enable nextcloud-scan-files.timer
